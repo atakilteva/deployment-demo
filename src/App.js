0d059bcb832ed27444783b5265bafa8d820b3e32
atakilteva <104560979+atakilteva@users.noreply.github.com>
2022-05-03 11:23:29 +0300

    kkk

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     
     <div className="App">
       <div className='header'>
-      <h1>Home</h1> 
-      <h1>About</h1>
+      <h1>Home</h1> <br></br>
+      <h1>About</h1><br></br>
       <h1>Contact</h1>
       </div>
       

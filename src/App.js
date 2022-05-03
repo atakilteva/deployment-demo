d7204b57a80056a8962ccf1f6e531c6e2c9bd836
atakilteva <104560979+atakilteva@users.noreply.github.com>
2022-05-03 11:01:42 +0300

    second add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         React Deployment is happing here!
+         React Deployment is happing!
         </p>
         <a
           className="App-link"
